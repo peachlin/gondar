592a0a3e98933fe13809cd81bfb968ac7294a15e
wangkun_ext <kun_wang@asdc.com.cn>
2019-06-11 20:43:54 +0800

    添加示例工程

---
+++ gondar-example/src/main/java/com/zmsj/gondar/example/ExampleApplication.java
@@ -0,0 +1,20 @@
+package com.zmsj.gondar.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author wang.kun
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ExampleApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(ExampleApplication.class, args);
+  }
+}
