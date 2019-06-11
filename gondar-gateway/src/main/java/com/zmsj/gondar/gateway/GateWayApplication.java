1ad99d20227a9741a04be91d33c2ef995397bcb3
wangkun_ext <kun_wang@asdc.com.cn>
2019-06-11 20:58:40 +0800

    zuul集成oauth2

--- gondar-gateway/src/main/java/com/zmsj/gondar/gateway/GateWayApplication.java
+++ gondar-gateway/src/main/java/com/zmsj/gondar/gateway/GateWayApplication.java
@@ -2,14 +2,12 @@ package com.zmsj.gondar.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author wangk
  */
-@EnableOAuth2Sso
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
