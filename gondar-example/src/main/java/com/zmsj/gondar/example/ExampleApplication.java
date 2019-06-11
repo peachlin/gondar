3333708a5d0630e025a2c05324a74078424849c9
wangkun_ext <kun_wang@asdc.com.cn>
2019-06-11 21:11:21 +0800

    springcloud 改为e版本

--- gondar-example/src/main/java/com/zmsj/gondar/example/ExampleApplication.java
+++ gondar-example/src/main/java/com/zmsj/gondar/example/ExampleApplication.java
@@ -3,7 +3,7 @@ package com.zmsj.gondar.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * @author wang.kun
